b831a24e3b7c4a290b65415c8962884451e47de2
Wolfgang Eibner <we@x-net.at>
2016-03-17 12:52:55 +0100

    Deploy in Tomcat and local

--- src/main/java/crac/controllers/MainController.java
+++ src/main/java/crac/controllers/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-  @RequestMapping("/")
+  @RequestMapping("/test")
   @ResponseBody
   public String index() {
 	  return "Hello World! Spring Boot + Hibernate! <br/> Click on <a href=\"hello-world/getAll\">me</a> to get all safed HelloWorld-data sets";
