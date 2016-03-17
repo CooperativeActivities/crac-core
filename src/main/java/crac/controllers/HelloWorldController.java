9c393aaf13e7a54b708c8ce23fe4d761d477f654
Wolfgang Eibner <we@x-net.at>
2016-03-17 10:44:51 +0100

    Unused Imports

--- src/main/java/crac/controllers/HelloWorldController.java
+++ src/main/java/crac/controllers/HelloWorldController.java
@@ -3,13 +3,9 @@ package crac.controllers;
 import crac.models.HelloWorld;
 import crac.models.HelloWorldDAO;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
