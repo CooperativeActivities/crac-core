158203270cb327027d5289e0e314c5d6d850f766
Wolfgang Eibner <we@x-net.at>
2016-03-17 12:19:39 +0100

    Changes for tomcat deploy

--- src/main/java/crac/Application.java
+++ src/main/java/crac/Application.java
@@ -1,10 +1,16 @@
 package crac;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
