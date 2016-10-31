980d2795a318fa433d0addbe6af322320c87c420
KasnocknDave <david.hondl@gmx.net>
2016-10-31 20:01:16 +0100

    fix

--- src/main/java/crac/filters/CorsFilter.java
+++ src/main/java/crac/filters/CorsFilter.java
@@ -25,8 +25,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, X-Custom-Header,Accept, Authorization");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, X-Custom-Header,Accept, Token");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, X-Custom-Header, Accept, Authorization, Token");
         //chain.doFilter(req, res);
         HttpServletRequest httpRequest = (HttpServletRequest) req;        
         if(!httpRequest.getMethod().equalsIgnoreCase("OPTIONS")){
