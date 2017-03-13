a398829e90edabfcd834b2f2a8d7d55a893f6334
KasnocknDave <david.hondl@gmx.net>
2017-03-13 14:41:37 +0100

    testdata

--- src/main/java/crac/controllers/SynchronizationController.java
+++ src/main/java/crac/controllers/SynchronizationController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -364,11 +366,118 @@ public class SynchronizationController {
 		System.out.println("||||DATA SYNCED||||");
 		System.out.println("-------------------------------");
 
+		addSamples();
 		addRoles();
 		addCompetenceRelationshipTypes();
 		addCompetencePermissionType();
 		addUsers();
 		return JSonResponseHelper.successFullAction("Data has been synced");
+	}
+	
+	private void addSamples(){
+		
+		UsernamePasswordAuthenticationToken userDetails = (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+		CracUser myUser = userDAO.findByName(userDetails.getName());
+		
+		Competence basicHumanSkills = new Competence();
+		basicHumanSkills.setCreator(myUser);
+		basicHumanSkills.setDescription("The majority of people is able to do these things.");
+		basicHumanSkills.setName("basic human skills");
+		
+		Competence breathing = new Competence();
+		breathing.setCreator(myUser);
+		breathing.setDescription("Beeing to stay alive by inhaling air.");
+		breathing.setName("breathing");
+
+		CompetenceRelationship basic_breathing = new CompetenceRelationship();
+		basic_breathing.setCompetence1(basicHumanSkills);
+		basic_breathing.setCompetence2(breathing);
+		basic_breathing.setType(competenceRelationshipTypeDAO.findOne((long) 1));
+
+		Competence walking = new Competence();
+		walking.setCreator(myUser);
+		walking.setDescription("Getting slowly from one point to another using human legs.");
+		walking.setName("walking");
+
+		CompetenceRelationship basic_walking = new CompetenceRelationship();
+		basic_walking.setCompetence1(basicHumanSkills);
+		basic_walking.setCompetence2(walking);
+		basic_walking.setType(competenceRelationshipTypeDAO.findOne((long) 2));
+		
+		Competence swimming = new Competence();
+		swimming.setCreator(myUser);
+		swimming.setDescription("Not drowning while in water.");
+		swimming.setName("swimming");
+
+		CompetenceRelationship basic_swimming = new CompetenceRelationship();
+		basic_swimming.setCompetence1(basicHumanSkills);
+		basic_swimming.setCompetence2(swimming);
+		basic_swimming.setType(competenceRelationshipTypeDAO.findOne((long) 3));
+		
+		Competence programming = new Competence();
+		programming.setCreator(myUser);
+		programming.setDescription("Beeing able to write computer programs.");
+		programming.setName("programming");
+
+		Competence javascriptProgramming = new Competence();
+		javascriptProgramming.setCreator(myUser);
+		javascriptProgramming.setDescription("Beeing able to write computer programs with/in JavaScript and it's libraries.");
+		javascriptProgramming.setName("javascript-programming");
+
+		CompetenceRelationship programming_javascriptProgramming = new CompetenceRelationship();
+		programming_javascriptProgramming.setCompetence1(programming);
+		programming_javascriptProgramming.setCompetence2(javascriptProgramming);
+		programming_javascriptProgramming.setType(competenceRelationshipTypeDAO.findOne((long) 2));
+		
+		Competence phpProgramming = new Competence();
+		phpProgramming.setCreator(myUser);
+		phpProgramming.setDescription("Beeing able to write computer programs with/in PHP and it's libraries.");
+		phpProgramming.setName("php-programming");
+
+		CompetenceRelationship programming_phpProgramming = new CompetenceRelationship();
+		programming_phpProgramming.setCompetence1(programming);
+		programming_phpProgramming.setCompetence2(phpProgramming);
+		programming_phpProgramming.setType(competenceRelationshipTypeDAO.findOne((long) 2));
+		
+		competenceDAO.save(basicHumanSkills);
+		competenceDAO.save(breathing);
+		competenceDAO.save(walking);
+		competenceDAO.save(swimming);
+		competenceDAO.save(programming);
+		competenceDAO.save(javascriptProgramming);
+		competenceDAO.save(phpProgramming);
+		
+		competenceRelationshipDAO.save(basic_breathing);
+		competenceRelationshipDAO.save(basic_walking);
+		competenceRelationshipDAO.save(basic_swimming);
+		competenceRelationshipDAO.save(programming_javascriptProgramming);
+		competenceRelationshipDAO.save(programming_phpProgramming);
+		
+		CracUser Webmaster = new CracUser();
+		
+		BCryptPasswordEncoder bcryptEncoder = new BCryptPasswordEncoder();
+		
+		Webmaster.setName("Webmaster");
+		Webmaster.setFirstName("Max");
+		Webmaster.setLastName("Mustermann");
+		Webmaster.setPassword(bcryptEncoder.encode("noOneKnowsThisPassword!1!1"));
+		//Webmaster.setRole(Role.USER);
+		Webmaster.setPhone("0987656789098");
+		Webmaster.setEmail("Webmaster@internet.at");
+		userDAO.save(Webmaster);
+	
+		CracUser AverageHuman = new CracUser();
+		
+		AverageHuman.setName("AverageHuman");
+		AverageHuman.setFirstName("Hans");
+		AverageHuman.setLastName("Musterhans");
+		AverageHuman.setPassword(bcryptEncoder.encode("noOneKnowsThisPasswordAnyway!1!1"));
+		//AverageHuman.setRole(Role.USER);
+		AverageHuman.setPhone("35678987654");
+		AverageHuman.setEmail("AverageHuman@internet.at");
+		userDAO.save(AverageHuman);
+
+
 	}
 	
 	private void addRoles(){
