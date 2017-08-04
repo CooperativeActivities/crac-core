c16f9fc08f5e7f1b5162a700d2eeb85d1d012c2e
KasnocknDave <david.hondl@gmx.net>
2017-08-04 10:03:07 +0200

    back

--- src/main/java/crac/Application.java
+++ src/main/java/crac/Application.java
@@ -3,16 +3,13 @@ package crac;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@SpringBootApplication(exclude = {EmbeddedServletContainerAutoConfiguration.class, 
-		WebMvcAutoConfiguration.class})
+@SpringBootApplication
 /**
  * main class for running the application, this file must be executed
  */
