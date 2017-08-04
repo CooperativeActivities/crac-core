7074025d9f97a7e41899121463218c6240c5af5d
KasnocknDave <david.hondl@gmx.net>
2017-08-04 11:00:32 +0200

    test

--- src/main/java/crac/Application.java
+++ src/main/java/crac/Application.java
@@ -3,6 +3,8 @@ package crac;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,10 +15,17 @@ import org.springframework.context.annotation.Configuration;
 /**
  * main class for running the application, this file must be executed
  */
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<Application> applicationClass = Application.class;
 
 }
