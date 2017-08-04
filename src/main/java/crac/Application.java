34522a455f29382fc66eea812d6a714e101b3ce6
KasnocknDave <david.hondl@gmx.net>
2017-08-04 09:47:46 +0200

    boot disabled

--- src/main/java/crac/Application.java
+++ src/main/java/crac/Application.java
@@ -3,13 +3,16 @@ package crac;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(exclude = {EmbeddedServletContainerAutoConfiguration.class, 
+		WebMvcAutoConfiguration.class})
 /**
  * main class for running the application, this file must be executed
  */
